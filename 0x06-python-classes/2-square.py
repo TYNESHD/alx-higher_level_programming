dc4f377acd85186a2f5c96c6755d5ae4f84321f2
TYNESHD <tinashedubem@gmail.com>
2024-02-21 08:27:20 -0800

    a class Square that defines a square by

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -10,10 +10,16 @@ class Square:
     >>> square_2 = Square(7)
     """
 
-    def __init__(self, size: int) -> None:
+    def __init__(self, size=0) -> None:
         """
-        Innitialize the size of the square
+        Innitialize the size of the square. the size can be specified.
+        If they are not, the size defaults to 0
 
-        :param size: int size of square
+        :param size: int size of square ( > 0)
         """
+        if (type(size) is not int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+
         self.__size = size
